2d7d94e10fa26a29517d55a26101c9c5d65244cf
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-15 11:07:13 +0100

    Initial source code customization (#11)

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+import App from "./components/App";
+
+import { APPLICATION_NAME } from "./constants/app.constants";
+
+import "./index.css";
+
+const root: ReactDOM.Root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App applicationName={APPLICATION_NAME} />
+  </React.StrictMode>,
 );
