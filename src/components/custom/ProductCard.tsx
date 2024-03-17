ff1db3b024da59e5211e9ff021c25bd57ba52aa1
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-17 19:06:59 +0100

    Create first cut of Warehouse page.

---
+++ src/components/custom/ProductCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Card from "../common/Card";
+
+import noImage from "../../images/no-image.png";
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const { id, name, supplierName, unit, photo, onStock } = product;
+
+  const cardContents: KeyValuePair<string, string>[] = [
+    { key: "supplierName", value: `Beszállító: ${supplierName}` },
+    { key: "onStock", value: `Raktáron: ${onStock} ${unit}` },
+  ];
+
+  return (
+    <Card
+      key={id}
+      title={name}
+      image={photo ? "" : noImage} // TODO: Later render the actual image of the product.
+      imageAlt={name}
+      contents={cardContents}
+    />
+  );
+};
+
+export default ProductCard;
