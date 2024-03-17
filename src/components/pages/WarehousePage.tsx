3872aee92897975e0d5fe9fb40cd8cbba8954a98
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-03-17 19:14:46 +0100

    Warehouse page initial design (#15)

--- src/components/pages/WarehousePage.tsx
+++ src/components/pages/WarehousePage.tsx
@@ -1,7 +1,48 @@
 import React from "react";
 
-const WarehousePage: React.FC<PageProps> = ({ pageName }) => {
-  return <h1>{pageName}</h1>;
+import ProductCard from "../custom/ProductCard";
+
+const WarehousePage: React.FC<PageProps> = () => {
+  // TODO: Clean up mock data after GraphQL backend is set up.
+  const products: Product[] = [
+    {
+      id: 1,
+      name: "6-os csavar",
+      supplierName: "ACME Corp.",
+      price: 150,
+      unit: "darab",
+      photo: null,
+      onStock: 1000,
+    },
+    {
+      id: 2,
+      name: "6-os csavar alátét (sima)",
+      supplierName: "ACME Corp.",
+      price: 20,
+      unit: "darab",
+      photo: null,
+      onStock: 1000,
+    },
+    {
+      id: 3,
+      name: "6-os csavar alátét (rugós)",
+      supplierName: "ACME Corp.",
+      price: 25,
+      unit: "darab",
+      photo: null,
+      onStock: 1000,
+    },
+  ];
+
+  return (
+    <section className="row">
+      {products.map((product: Product) => (
+        <article key={product.id} className="col-sm-12 col-md-6 col-lg-4">
+          <ProductCard product={product} />
+        </article>
+      ))}
+    </section>
+  );
 };
 
 export default WarehousePage;
--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -5,6 +5,13 @@ declare module "*.jpg";
 
 // Common FC Props types
 
+interface CardProps {
+  title: string;
+  image: string;
+  imageAlt: string;
+  contents: KeyValuePair<string, string>[];
+}
+
 interface NavbarProps {
   image: string;
   pages: KeyValuePair<string, string>[];
@@ -18,6 +25,22 @@ interface AppProps {
 
 interface PageProps {
   pageName: string;
+}
+
+interface ProductCardProps {
+  product: Product;
+}
+
+// Custom data model types
+
+interface Product {
+  id: number;
+  name: string;
+  supplierName: string;
+  price: double;
+  unit: string;
+  photo: string | null;
+  onStock: number;
 }
 
 // Other custom types
