5125e015343515fb760576362b0531f1dda7b60a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-04-04 19:54:10 +0200

    Display Axios error message on the UI when photo upload was not successful.

--- src/components/custom/ProductCard.tsx
+++ src/components/custom/ProductCard.tsx
@@ -1,6 +1,8 @@
 import React, { ChangeEvent, useState } from "react";
+import { AxiosError } from "axios";
 
 import Card from "../common/Card";
+import ErrorMessage from "../common/ErrorMessage";
 
 import { uploadPhoto } from "../../http/productPhotoService";
 
@@ -8,8 +10,14 @@ import noImage from "../../images/no-image.png";
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const [uploadedPhotoFileName, setUploadedPhotoFileName] = useState("");
+  const [shouldShowErrorMessage, setShouldShowErrorMessage] = useState({
+    shouldShow: false,
+    errorMessage: "",
+  });
 
-  const handlePhotoUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+  const handlePhotoUpload = async (
+    event: ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
     try {
       if (!event.target.files || event.target.files.length === 0) {
         return;
@@ -21,8 +29,14 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       const { data: fileName } = await uploadPhoto(id, formData);
 
       setUploadedPhotoFileName(fileName);
-    } catch (error) {
-      console.error(error); // TODO: Later add descriptive error message on the UI.
+      setShouldShowErrorMessage({ shouldShow: false, errorMessage: "" });
+
+      // @ts-ignore
+    } catch (axiosError: AxiosError) {
+      setShouldShowErrorMessage({
+        shouldShow: true,
+        errorMessage: axiosError.response.data,
+      });
     }
   };
 
@@ -40,17 +54,22 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   }
 
   return (
-    <Card
-      key={id}
-      title={name}
-      image={image}
-      imageAlt={name}
-      contents={[
-        { key: 1, value: `Beszállító: ${supplierName}` },
-        { key: 2, value: `Raktáron: ${onStock} ${unit}` },
-      ]}
-      onChange={handlePhotoUpload}
-    />
+    <React.Fragment>
+      <Card
+        key={id}
+        title={name}
+        image={image}
+        imageAlt={name}
+        contents={[
+          { key: 1, value: `Beszállító: ${supplierName}` },
+          { key: 2, value: `Raktáron: ${onStock} ${unit}` },
+        ]}
+        onChange={handlePhotoUpload}
+      />
+      {shouldShowErrorMessage.shouldShow && (
+        <ErrorMessage message={shouldShowErrorMessage.errorMessage} />
+      )}
+    </React.Fragment>
   );
 };
 
