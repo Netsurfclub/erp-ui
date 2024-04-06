eba2d55139de7af97f90a0edb39b7cb66c96ad05
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-04-06 10:39:14 +0200

    Refactor ErrorMessage to use toast notification (#28)

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -1,4 +1,8 @@
+// Application properties constants
+
 const APPLICATION_NAME: string = "erp";
+
+// Page name constants
 
 const MAIN_PAGE: KeyValuePair<string, string> = {
   key: "Főoldal",
@@ -15,4 +19,29 @@ const ERROR_PAGE: KeyValuePair<string, string> = {
   value: "/error",
 };
 
-export { APPLICATION_NAME, MAIN_PAGE, WAREHOUSE_PAGE, ERROR_PAGE };
+// User-friendly message constants
+
+const PHOTO_UPLOAD_PROGRESS_MESSAGE: string = "Fénykép feltöltése...";
+const PHOTO_UPLOAD_SUCCESS_MESSAGE: string = "Fénykép feltötése sikeres volt.";
+const PHOTO_UPLOAD_ERROR_MESSAGE: string = "Fénykép feltötése sikertelen volt.";
+const NETWORK_ERROR_CODE: string = "ERR_NETWORK";
+const INTERNET_CONNECTION_ERROR_MESSAGE: string = "Nincs internetkapcsolat.";
+
+// Miscellaneous constants
+
+const FORM_DATA_NAME_FILE: string = "file";
+const TOAST_NOTIFICATION_DURATION: number = 2500;
+
+export {
+  APPLICATION_NAME,
+  MAIN_PAGE,
+  WAREHOUSE_PAGE,
+  ERROR_PAGE,
+  PHOTO_UPLOAD_PROGRESS_MESSAGE,
+  PHOTO_UPLOAD_SUCCESS_MESSAGE,
+  PHOTO_UPLOAD_ERROR_MESSAGE,
+  NETWORK_ERROR_CODE,
+  INTERNET_CONNECTION_ERROR_MESSAGE,
+  FORM_DATA_NAME_FILE,
+  TOAST_NOTIFICATION_DURATION,
+};
