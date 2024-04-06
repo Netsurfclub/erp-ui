fd3d927035f801fb27927b4b6a2a03d24ce27b0f
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-04-06 10:50:01 +0200

    Implement common components (#18)

--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -5,6 +5,13 @@ declare module "*.jpg";
 
 // Common FC Props types
 
+interface ButtonProps {
+  text: string;
+  url: string;
+  width?: string;
+  disabled?: boolean;
+}
+
 interface CardProps {
   title: string;
   image: string;
@@ -13,15 +20,76 @@ interface CardProps {
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
+interface DropdownProps {
+  name: string;
+  value: string;
+  labelText: string;
+  data: any[];
+  errorMessage: string;
+  onChange: ChangeEventHandler<HTMLSelectElement>;
+}
+
+interface ErrorMessageProps {
+  message: string;
+}
+
 interface FileInputProps {
   name: string;
   disabled: boolean;
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
+interface ListGroupProps {
+  collection: any[];
+}
+
+interface ModalWindowProps {
+  title: string;
+  content: string;
+  buttonText: string;
+  routingButtonText: string;
+  redirectUrl: string;
+  isSuccessModal: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
 interface NavbarProps {
   image: string;
   pages: KeyValuePair<string, string>[];
+}
+
+interface NumberAddOnInputProps {
+  name: string;
+  value: string;
+  minValue: number;
+  addOnText: string;
+  errorMessage: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+interface NumberInputProps {
+  name: string;
+  value: string;
+  minValue: number;
+  labelText: string;
+  errorMessage: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+interface TableProps {
+  caption: string;
+  columns: any[];
+  collection: any[];
+}
+
+interface TextInputProps {
+  type: string;
+  name: string;
+  value: string;
+  maxLength: number;
+  labelText: string;
+  errorMessage: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 // Custom FC and Page FC Props types
