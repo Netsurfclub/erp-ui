10711f1b8355a71d942d726dbefa52a9a044d0a5
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-04-11 19:30:56 +0200

    Statically define localhost port to port 3000.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ const env: Record<string, string> = loadEnv("mode", process.cwd(), "");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   define: {
     "process.env.VITE_ERP_API_GRAPHQL_ENDPOINT": JSON.stringify(
       env.VITE_ERP_API_GRAPHQL_ENDPOINT,
