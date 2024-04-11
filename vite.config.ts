765175acf74a7487f878ff969fd23e3a4269cd26
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-04-11 20:08:59 +0200

    Add 'strict-port' property to Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const env: Record<string, string> = loadEnv("mode", process.cwd(), "");
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  define: {
-    "process.env.VITE_ERP_API_GRAPHQL_ENDPOINT": JSON.stringify(
-      env.VITE_ERP_API_GRAPHQL_ENDPOINT,
-    ),
-    "process.env.VITE_PHOTO_UPLOAD_ENDPOINT": JSON.stringify(
-      env.VITE_PHOTO_UPLOAD_ENDPOINT,
-    ),
-    "process.env.VITE_PHOTO_RETRIEVE_ENDPOINT": JSON.stringify(
-      env.VITE_PHOTO_RETRIEVE_ENDPOINT,
-    ),
-  },
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env: Record<string, string> = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      port: 3000,
+      strictPort: true,
+    },
+    define: {
+      "process.env.VITE_ERP_API_GRAPHQL_ENDPOINT": JSON.stringify(
+        env.VITE_ERP_API_GRAPHQL_ENDPOINT,
+      ),
+      "process.env.VITE_PHOTO_UPLOAD_ENDPOINT": JSON.stringify(
+        env.VITE_PHOTO_UPLOAD_ENDPOINT,
+      ),
+      "process.env.VITE_PHOTO_RETRIEVE_ENDPOINT": JSON.stringify(
+        env.VITE_PHOTO_RETRIEVE_ENDPOINT,
+      ),
+    },
+    plugins: [react()],
+  };
 });
