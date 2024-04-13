5e1ca72dec6eb112e5b9c5a7691b6aaa57a548d1
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-04-13 12:55:32 +0200

    Remove temp. variables

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,28 +18,26 @@ import logo from "../assets/logo.png";
 const App: React.FC<AppProps> = () => {
   const pages: KeyValuePair<string, string>[] = [MAIN_PAGE, WAREHOUSE_PAGE];
 
-  const mainPageComponent: JSX.Element = <MainPage pageName={MAIN_PAGE.key} />;
-  const warehousePageComponent: JSX.Element = (
-    <WarehousePage pageName={WAREHOUSE_PAGE.key} />
-  );
-  const errorPageComponent: JSX.Element = (
-    <ErrorPage pageName={ERROR_PAGE.key} />
-  );
-  const navigationComponentToErrorPage: JSX.Element = (
-    <Navigate to={ERROR_PAGE.value} />
-  );
-
   return (
     <BrowserRouter>
       <Navbar image={logo} pages={pages} />
       <main className="container">
         <Routes>
-          <Route path={MAIN_PAGE.value} element={mainPageComponent} />
-          <Route path={WAREHOUSE_PAGE.value} element={warehousePageComponent} />
-          <Route path={ERROR_PAGE.value} element={errorPageComponent} />
+          <Route
+            path={MAIN_PAGE.value}
+            element={<MainPage pageName={MAIN_PAGE.key} />}
+          />
+          <Route
+            path={WAREHOUSE_PAGE.value}
+            element={<WarehousePage pageName={WAREHOUSE_PAGE.key} />}
+          />
+          <Route
+            path={ERROR_PAGE.value}
+            element={<ErrorPage pageName={ERROR_PAGE.key} />}
+          />
           <Route
             path={ALL_PATHS.value}
-            element={navigationComponentToErrorPage}
+            element={<Navigate to={ERROR_PAGE.value} />}
           />
         </Routes>
       </main>
